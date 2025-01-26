37e44c335784251667a95fd9c1f34d34313bcfeb
Umar  Faizan <numl-s22-29670@numls.edu.pk>
2025-01-26 16:57:54 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ with st.expander('Data'):
   y = df.species
   y
 
-with st.expander('Data Visualization'):
-  st.scatter_chart(data=df,x='bill_length_mm',y='bill_depth_mm',colour='species')
+with st.expander('Data visualization'):
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
 
 
