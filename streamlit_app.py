41a67a453998cb60bf0659c6699aee699c0c4724
Umar  Faizan <numl-s22-29670@numls.edu.pk>
2025-01-26 18:56:15 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,7 +93,7 @@ clf.fit(X, y)
 ## Apply model to make predictions
 prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
-prediction_proba[0]
+prediction_proba[1]
 
 df_prediction_proba = pd.DataFrame(prediction_proba)
 df_prediction_proba.columns = ['Adelie', 'Chinstrap', 'Gentoo']
