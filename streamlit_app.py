396897bbc550c7b07ef062171a5f60cdb0c23763
Umar  Faizan <numl-s22-29670@numls.edu.pk>
2025-01-26 16:37:46 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title('🤖 Machine Learning App')
 st.info('This is an app builds a Machine Learning Model!')
 
 df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
-df
+df.head()
 
 
 
