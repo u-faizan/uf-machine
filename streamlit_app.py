eb690e1777564930a1072585a537be8e58ce3e21
Umar  Faizan <numl-s22-29670@numls.edu.pk>
2025-01-26 18:48:39 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,6 +96,9 @@ prediction_proba = clf.predict_proba(input_row)
 
 df_prediction_proba = pd.DataFrame(prediction_proba)
 df_prediction_proba.columns = ['Adelie', 'Chinstrap', 'Gentoo']
+df_prediction_proba.rename(columns={0: 'Adelie',
+                                 1: 'Chinstrap',
+                                 2: 'Gentoo'})
 df_prediction_proba
 
 
